e72e18a7cf8d2657386f347b6fddd00284954bdb
jamiejohnsonkc <jamiejohnsonkc@gmail.com>
2020-12-10 09:59:29 -0600

    updating package & gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,1 +1,14 @@
-module.exports = { plugins: [] }
+module.exports = { plugins: [
+  `gatsby-plugin-theme-ui`,
+  `gatsby-theme-jim-theme-ui`,
+  {
+    resolve: 'gatsby-plugin-react-svg',
+    options: {
+      name: `svg`,
+      rule: {
+        include: /assets/,
+      },
+    },
+  },
+],
+}
